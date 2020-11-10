c24c6adcf3a8f0bf59c28ea0a04723cfa81336ad
theogf <theo.galyfajou@gmail.com>
2020-11-10 16:09:03 +0000

    Deploying to gh-pages from  @ 3330513e0d3fc71843949ad11b5186f07dac776c 🚀

--- assets/blogposts/gpderivations/code/derivgp/ex1.jl
+++
@@ -1,6 +0,0 @@
-# This file was generated, do not modify it. # hide
-d2KL_dSS(Σ) = 0.5*kron(inv(Σ),inv(Σ))
- analytic = d2KL_dSS(Σ)
-# @show autodiff = ForwardDiff.hessian(Σ->KL(μ,Σ,μ₀,K),Σ)
-# plot(heatmap(analytic,title="Analytic"),heatmap(autodiff,title="AutoDiff"),heatmap(analytic-autodiff,title="Difference"),yflip=true,layout=(1,3)) # hide
-# savefig(joinpath(@OUTPUT,"d2KLdSigmaSigma.svg")) # hide
